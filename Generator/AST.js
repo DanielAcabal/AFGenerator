c3ee0d64afa1bfeff2d80b538f8480092f403bab
DanielAcabal <danchiacabal@gmail.com>
2023-05-18 22:04:03 -0600

    Added transitions table

--- Generator/AST.js
+++ Generator/AST.js
@@ -1,16 +1,85 @@
 class AST {
   constructor(statements) {
     this.statements = statements;
-    this.trees = []
-    this.follows = []
+    this.trees = [];
+    this.follows = [];
+    this.terminals = [];
+    this.transitionTables = [];
   }
   Generate() {
-    this.statements.forEach((element) => {
-      const follows = []
-      const result = element.Generate(follows);
-      this.trees.push(result)
-      this.follows.push(follows)
+    this.statements.forEach((element, index) => {
+      const follows = [];
+      const terminal = new Map();
+      const result = element.Generate(follows, terminal);
+      this.trees.push(result);
+      this.follows.push(follows);
+      this.terminals.push(terminal);
+      this.transitionTables.push([]);
+      this.createTransitionTable(index);
     });
   }
+  createTransitionTable(index) {
+    if (index >= this.follows.length) return;
+    if (index >= this.trees.length) return;
+    if (index >= this.terminals.length) return;
+    if (index >= this.transitionTables.length) return;
+    const followsTable = this.follows[index];
+    const tree = this.trees[index];
+    const terminal = this.terminals[index];
+    const transitionTable = this.transitionTables[index];
+
+    const X0 = [...tree.firstPos];
+    const states = [X0];
+
+    let n = 0;
+    while (n < states.length) {
+      const reduced = this.reduceStates(states[n], followsTable);
+      this.createStates(reduced, states, terminal, transitionTable);
+      n++;
+    }
+  }
+  reduceStates(idFollows, followsTable) {
+    const reduced = new Map();
+    // Reducing States
+    idFollows.forEach((element) => {
+      // Looking for terminal in followsTable by id
+      const { value, follows } = followsTable.find((value) =>
+        value.id === element
+      );
+      if (reduced.has(value)) {
+        const previousFollow = reduced.get(value);
+        const newFollow =  [...new Set(previousFollow.concat(follows))]
+        reduced.set(value, newFollow.sort((a,b)=>a-b));
+      } else {
+        reduced.set(value, [...follows]);
+      }
+    });
+    return reduced;
+  }
+  createStates(reduced, states, terminal, transitionTable) {
+    // Checking news states
+    const newState = new Map(terminal);
+    reduced.forEach((element, index) => {
+      const has = states.findIndex((value) => compareLists(value, element));
+      if (has < 0 && element.length > 0) {
+        // Adding new state
+        states.push(element);
+        // Creating state in transitionTable
+        newState.set(index, states.length - 1);
+      } else {
+        // Pointing state
+        newState.set(index, has);
+      }
+    });
+    transitionTable.push(newState);
+    
+  }
 }
-exports.AST = AST
+function compareLists(list1, list2) {
+  if (list1.length != list2.length) return false;
+  for (let index = 0; index < list1.length; index++) {
+    if (list1[index] !== list2[index]) return false;
+  }
+  return true;
+}
+exports.AST = AST;
--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const { argv } = require("node:process");
 const parser = require("./Grammar/grammar.js");
 const { AST } = require("./Generator/AST.js");
 
-//const result = parser.parse("[ ( a.b | a.c )+ .d.a? ] # [a|b]#");
+//const result = parser.parse("[ a .( a | b )* .b ] #")
+//const result = parser.parse("[ ( a.b | a.c )+ .d.a? ] # ");
 //const result = parser.parse("[L+|N+|L.(L|N)*|c.(L|N|R)*.c|S+]#")
-const result = parser.parse("[s?.0*.(d+|d*.p.d+)]#")
+//const result = parser.parse("[s?.0*.(d+|d*.p.d+)]#")
+//const result = parser.parse("[(a+|a+|a.b.c+)+.c*]#")
+//const result = parser.parse("[a*.(a.b+).b.a+.(b|a)*.b]#")
+//const result = parser.parse("[a.a+.b|c.b*]#[a|b|c?.a+.b*]# [a+.b+.c+|a]#")
+const result = parser.parse("[(a+.b|b.a+|a+.a|b.b+)*.c]#")
+
 const ast = new AST(result);
 ast.Generate();
-//console.log(ast.trees[0])
-//console.log("Follows: ",ast.follows[0])
-
+//console.log("Treess:",ast.trees[0])
+console.log("Follows: ",ast.follows[0])
+ast.transitionTables[0].forEach((element,index) => {
+  console.log(index,element)
+});
 argv.forEach((value, index) => {
   console.log(index, value);
 });
